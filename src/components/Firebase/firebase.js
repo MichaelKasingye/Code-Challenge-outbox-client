87db00ca4e611a5b6962f1e4bf1568f83c7c186f
michaelkasingye <michaelkasingye@gmail.com>
2021-07-13 18:54:53 +0300

    add access

--- src/components/Firebase/firebase.js
+++ src/components/Firebase/firebase.js
@@ -2,13 +2,13 @@ import firebase from "firebase";
 
 const firebaseApp = firebase.initializeApp({
    
-    apiKey: "AIzaSyCMuBipTWD9L8R-lU3n_kv8n3VAFySD3gE",
-    authDomain: "ecommerce-3b623.firebaseapp.com",
-    projectId: "ecommerce-3b623",
-    storageBucket: "ecommerce-3b623.appspot.com",
-    messagingSenderId: "88818567063",
-    appId: "1:88818567063:web:1723619ef0db88b494e6d2",
-    measurementId: "G-WSEVVWLDES"
+    apiKey: "AIzaSyBfOiVvGEKbWlfzip4NOgS3uVWHlzYzQV0",
+    authDomain: "challengecodeoutbox.firebaseapp.com",
+    projectId: "challengecodeoutbox",
+    storageBucket: "challengecodeoutbox.appspot.com",
+    messagingSenderId: "845990247280",
+    appId: "1:845990247280:web:1c3a606477704c74432db5",
+    measurementId: "G-5GG9Q6ZW33"
      
 });
 
